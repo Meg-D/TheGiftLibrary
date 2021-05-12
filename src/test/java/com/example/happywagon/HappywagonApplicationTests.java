2ba211e81255a16c93850b76c13d37941f3b071f
Meghna Dubey <meghnadubey300@gmail.com>
2021-05-12 12:23:23 +0530

    testing

--- src/test/java/com/example/happywagon/HappywagonApplicationTests.java
+++ src/test/java/com/example/happywagon/HappywagonApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.happywagon;
 
+import com.example.happywagon.bean.Artists;
+import com.example.happywagon.dao.ArtistDao;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HappywagonApplicationTests {
 
     @Test
     void contextLoads() {
     }
+    @Autowired
+    ArtistDao artistDao;
 
+    @Test
+    public void find_all_Artists(){
+        List<Artists> artistsList = artistDao.findAll();
+        int num= 4;
+        Assert.assertEquals(num,artistsList.size());
+    }
 }
+
+
